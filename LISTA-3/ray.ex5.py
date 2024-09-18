2c0c8d38acb47d1500b8e2d80ac7b52b1a152551
null <null>
2024-09-18 11:22:21 -0300

    exs

--- LISTA-3/ray.ex5.py
+++ LISTA-3/ray.ex5.py
@@ -1,10 +1,25 @@
 '''5. Escreva uma classe Point descrito por coordenadas reais x, y. Crie métodos get e set
+
+Buscar
+
+
+
+Nike
+TÊNIS DE CORRIDAEncontre o seu tênis ideal.Saiba Mais
+
+Nike
+CARTÃO PRESENTEpara presentes de última hora.Saiba Mais.
+
+Nike
+Camisa III do TimãoInspiradas nas mulheres negras do clubeSaiba Mais
+
+
 e faça um programa de teste para a sua classe. A seguir, crie e teste os seguintes
 métodos para a classe Ponto:
 a. O método que recebe como parâmetros um deslocamento dx e outro dy para
 movimentar o Ponto.
 b. O método que retorna a distância entre este ponto e outro dado como
-parâmetro.
+parâmetro. 
 Crie e teste um construtor para a classe Point, que inicialize x e y em 1, mas que
 também possa receber valores dados.'''
 import math
