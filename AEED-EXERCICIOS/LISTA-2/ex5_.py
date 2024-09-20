61b3b34ec396df0bedaa7092dc71544b981e5b97
Pedro Henrique Batista da Silva <pedrohmogi@hotmail.com>
2024-09-20 08:10:29 -0300

    arrumando

--- AEED-EXERCICIOS/LISTA-2/ex5_.py
+++ AEED-EXERCICIOS/LISTA-2/ex5_.py
@@ -2,18 +2,14 @@
 5. Dada a função abaixo, determine a complexidade:
 '''
 
-def tem_duplicacao(lista, n):
-    for i in range(n):
-        valor = lista[i]
-        for j in range(i+1, n):
-            if lista[j] == valor:
-                return True
-    return False
+def tem_duplicacao(array, n):
+    for i=0 to n-1:
+        val = array[i]
+        for j=(i+1) to n-1:
+            if array[j] == val:
+                return true
+    return false
     
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 8]
-n = len(lista)
-
-print(tem_duplicacao(lista, n)) #True
 
 #Pior caso: O(n²) (quando não há duplicatas, e todos os elementos precisam ser comparados).
 #Melhor caso: O(1) (se uma duplicação for encontrada imediatamente).
