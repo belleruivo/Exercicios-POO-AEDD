7a02ffee5d116928e531522a129c43047bb28795
Isabelle Ruivo dos Santos <isabelleruivosantos@gmail.com>
2024-09-28 09:20:07 -0300

    ML6-ML3

---
+++ AEED-EXERCICIOS/M3L/ex5-busca-num-vetor.py
@@ -0,0 +1,5 @@
+'''5. Faça um programa que cadastre n números, não permitindo números repetidos.
+Ordene-os e, em seguida, verifique se o número digitado pelo usuário está no vetor.
+Caso encontre, verifique se está numa posição par ou ímpar do vetor:
+a. Usando busca sequencial.
+b. Usando busca binária.'''
