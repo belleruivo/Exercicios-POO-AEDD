81f4098c0a450de1702c3d41496c13ddcb3f5c50
Isabelle Ruivo dos Santos <isabelleruivosantos@gmail.com>
2024-10-09 16:55:59 -0300

    Organização dos comentários

--- POO-EXERCICIOS/M8L/M5L-Escolhidos/ex1-VirtualStore.py
+++ POO-EXERCICIOS/M8L/M5L-Escolhidos/ex1-VirtualStore.py
@@ -1,17 +1,7 @@
 '''
 Escolha pelo menos 5 exercícios das listas M3L e M5L (5 de cada) para expandir o projeto do
-exercício, incluindo novas classes relacionadas, conforme a sua criatividade, aplicando a
-associação de classes.
+exercício, incluindo novas classes relacionadas, conforme a sua criatividade, demonstrando
+a injeção de dependência..
 
-Uma boa maneira de criar esse exercício é seguindo o seguinte roteiro:
-
-- descreva em no máximo 200 palavras o que é um objeto específico e o que ele faz. 
-- Liste os substantivos e verbos separadamente. Cada substantivo corresponde a um objeto que precisará ser
-construído para implementar um sistema. 
-- Selecione 5 dos objetos que você listou e, para cada um, liste vários atributos e comportamentos. 
-- Perceba também as associações e implemente-as.
-- Descreva brevemente como esses objetos interagem entre si e com outros objetos na sua descrição. 
-
-Estes passos que você seguiu são típicos do projeto orientado a objetos.
 '''
 
