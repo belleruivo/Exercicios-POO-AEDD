5c65b09d69044e7b82bc83f60656281229983878
RayaraChambela <rachambela@gmail.com>
2024-10-11 22:16:34 -0300

    ex 9 L7

--- POO-EXERCICIOS/M7L/M3L - OCP/ex9-date/date.py
+++ POO-EXERCICIOS/M7L/M3L - OCP/ex9-date/date.py
@@ -6,13 +6,6 @@ um set para cada variável. Forneça um método que exibe o dia, o mês e o ano
 separados por barras “/”. Teste a classe implementada e seus métodos.
 '''
 
-from datetime import datetime
-import calendar
-import locale
-
-# Configura a localidade para português do Brasil
-locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
-
 class Date:
     def __init__(self, dia, mes, ano):
         self.dia = dia
@@ -37,72 +30,7 @@ class Date:
     def set_ano(self, ano):
         self.ano = ano
         
+    def formatar_data(self, formatador):
+        return formatador.formatar(self)
 
-class FormatadorData:
-    def __init__(self, data):
-        self.data = data
-    
-    def formatar_data(self, formato="padrao"):
-        if formato == "iso":
-            return f"{self.data.ano}-{self.data.mes:02}-{self.data.dia:02}"
-        elif formato == "us":
-            return f"{self.data.mes:02}/{self.data.dia:02}/{self.data.ano}"
-        else:
-            return f"{self.data.dia:02}/{self.data.mes:02}/{self.data.ano}"
 
-def obter_dia(mes, ano):
-    while True:
-        try:
-            dia = int(input("Digite o dia: "))
-            dias_no_mes = calendar.monthrange(ano, mes)[1]  # Quantidade de dias no mês e ano especificados
-            if 1 <= dia <= dias_no_mes:
-                return dia
-            else:
-                # Obtém o nome do mês em português
-                nome_mes = datetime(ano, mes, 1).strftime('%B').capitalize()
-                print(f"Dia inválido! {nome_mes} de {ano} tem apenas {dias_no_mes} dias.")
-        except ValueError:
-            print("Entrada inválida. Por favor, insira um número inteiro.")
-
-def obter_mes():
-    while True:
-        try:
-            mes = int(input("Digite o mês: "))
-            if 1 <= mes <= 12:
-                return mes
-            else:
-                print("Mês inválido. Insira um valor entre 1 e 12.")
-        except ValueError:
-            print("Entrada inválida. Por favor, insira um número inteiro.")
-
-def obter_ano():
-    while True:
-        ano = input("Digite o ano (4 dígitos): ").strip()
-        if len(ano) == 4 and ano.isdigit():
-            return int(ano)  
-        else:
-            print("Entrada inválida. O ano deve ter exatamente 4 dígitos.\n")
-
-def main():
-    print("-=" * 30)
-    while True:
-        try:
-            ano = obter_ano()
-            mes = obter_mes()
-            dia = obter_dia(mes, ano)
-            
-            data = datetime(ano, mes, dia)
-            data_obj = Date(dia, mes, ano)
-            
-            # Usa o FormatadorData para formatos personalizados
-            formatador = FormatadorData(data_obj)
-            print("\nFormato ISO:", formatador.formatar_data("iso"))
-            print("Formato US:", formatador.formatar_data("us"))
-            print("Formato Padrão:", formatador.formatar_data())
-            break  
-        except ValueError:
-            print("Ops! Algo deu errado com a sua data. Por favor, insira uma data válida.\n")
-
-    print("-=" * 30)
-
-main()
