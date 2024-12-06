21c23af7b61a6f4a624d09dbd788507e4a6cf297
Pedro <pedrommachado256@gmail.com>
2024-12-06 09:26:52 -0300

    exercicios

--- AEED-EXERCICIOS/M7L/ex8/main.py
+++ AEED-EXERCICIOS/M7L/ex8/main.py
@@ -1,48 +1,51 @@
+# Crie um programa que ache a Arvore Geradora Mínima do grafo abaixo utilizando o
+# Algoritmo de Prim começando do vértice A.
+
 import heapq
 
-def prim_algorithm(graph, start):
-    num_vertices = len(graph)
-    visited = [False] * num_vertices  # Para rastrear vértices visitados
-    min_heap = [(0, start)]  # (peso, vértice)
-    mst = []  # Armazenamento da Árvore Geradora Mínima
-    total_cost = 0
+def algoritmo_prim(grafo, inicio):
+    num_vertices = len(grafo)
+    visitado = [False] * num_vertices  # Para rastrear vértices visitados
+    heap_minimo = [(0, inicio)]  # (peso, vértice)
+    agm = []  # Armazenamento da Árvore Geradora Mínima
+    custo_total = 0
 
-    while min_heap:
-        weight, current_vertex = heapq.heappop(min_heap)
+    while heap_minimo:
+        peso, vertice_atual = heapq.heappop(heap_minimo)
 
-        if visited[current_vertex]:
+        if visitado[vertice_atual]:
             continue
 
-        visited[current_vertex] = True
-        total_cost += weight
+        visitado[vertice_atual] = True
+        custo_total += peso
 
-        for neighbor in range(num_vertices):
-            if graph[current_vertex][neighbor] != 0 and not visited[neighbor]:
-                heapq.heappush(min_heap, (graph[current_vertex][neighbor], neighbor))
-                mst.append((current_vertex, neighbor, graph[current_vertex][neighbor]))
+        for vizinho in range(num_vertices):
+            if grafo[vertice_atual][vizinho] != 0 and not visitado[vizinho]:
+                heapq.heappush(heap_minimo, (grafo[vertice_atual][vizinho], vizinho))
+                agm.append((vertice_atual, vizinho, grafo[vertice_atual][vizinho]))
 
-    return mst, total_cost
+    return agm, custo_total
 
+# Grafo representado por uma matriz de adjacências
 grafo = [
-    [0, 15, 10, 19, 0, 0, 0, 0, 0, 0], # A
-     [15, 0, 0, 7, 17, 0, 0, 0, 0, 0],  # B
-     [10, 0, 0, 16, 0, 14, 0, 0, 0, 0],  # C
-     [19, 7, 16, 0, 12, 6, 3, 0, 0, 0], # D
-     [0, 17, 0, 12, 0, 0, 20, 13, 0, 0],# E
-     [0, 0, 14, 6, 0, 0, 9, 0, 5, 0],  # F
-     [0, 0, 0, 3, 20, 9, 0, 4, 1, 11],  # G
-     [0, 0, 0, 0, 13, 0, 4, 0, 0, 2],    # H
-     [0, 0, 0, 0, 0, 5, 1, 0, 0, 18],   # I
-     [0, 0, 0, 0, 0, 0, 11, 2, 18, 0]    # J
+    [0, 15, 10, 19, 0, 0, 0, 0, 0, 0],  # A
+    [15, 0, 0, 7, 17, 0, 0, 0, 0, 0],   # B
+    [10, 0, 0, 16, 0, 14, 0, 0, 0, 0],  # C
+    [19, 7, 16, 0, 12, 6, 3, 0, 0, 0],  # D
+    [0, 17, 0, 12, 0, 0, 20, 13, 0, 0], # E
+    [0, 0, 14, 6, 0, 0, 9, 0, 5, 0],    # F
+    [0, 0, 0, 3, 20, 9, 0, 4, 1, 11],  # G
+    [0, 0, 0, 0, 13, 0, 4, 0, 0, 2],   # H
+    [0, 0, 0, 0, 0, 5, 1, 0, 0, 18],  # I
+    [0, 0, 0, 0, 0, 0, 11, 2, 18, 0]   # J
 ]
 
-
 # Índice inicial (A = 0)
-start_vertex = 0
+vertice_inicial = 0
 # A árvore geradora mínima é uma árvore que conecta todos os vértices do grafo com o menor custo total possível, ou seja, 
 # o total das arestas que a compõem deve ser minimizado.
 
-def prim_agm(grafo, inicio):
+def agm_prim(grafo, inicio):
     n = len(grafo)
     visitado = [False] * n  # Lista para verificar se o vértice foi visitado
     agm = []  # Lista para armazenar as arestas da Árvore Geradora Mínima
@@ -84,8 +87,8 @@ def prim_agm(grafo, inicio):
     return agm
 
 
-agm = prim_agm(grafo, 0)
+agm = agm_prim(grafo, 0)
 
-print("Arestas da Arvore Geradora Minima:")
+print("Arestas da Árvore Geradora Mínima:")
 for u, v, peso in agm:
     print(f"{chr(u + 65)} - {chr(v + 65)} (peso: {peso})")
